5e7688060feb3acf7abc97bd17a7296febe5c16e
patrickclima2 <patrickc.lima2@gmail.com>
2022-04-08 11:20:07 -0300

    create layout transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Dashboard />
       <GlobalStyle />
     </>
